c561c84e0c44ab7b037d70ad294a55194d0bea64
Inamul <inamulhoque097@gmail.com>
2022-07-26 12:58:56 +0530

    Added: Email validation

--- form.js
+++ form.js
@@ -1,50 +1,11 @@
-const text = document.querySelector("#text");
-const textError = document.querySelector(".text-error");
-text.addEventListener('input', function () {
-    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,)$");
+function validation() {
+    var text = document.getElementById("text").value;
+    var password = document.getElementById("pwd").value;
+    var mail = document.getElementById("email").value;
+    var phone = document.getElementById("tel").value;
 
-    if (nameRegex.test(text.value)) {
-        textError.textContent = "";
+    var mailRegex = /^[a-z\\d+_.-]+@[a-z\\d.-]+$/;
+    if (!mailRegex.test(mail)) {
+        alert("Invalid e-mail");
     }
-    else textError.textContent = "Name is Incorrect";
-});
-
-const password = document.querySelector("#pwd");
-const passwordError = document.querySelector(".password-error");
-password.addEventListener('input', function () {
-    let pwdRegex = RegExp("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=])(?=\\S+$).{7,}$");
-    if (pwdRegex.test(pwdRegex.value)) {
-        passwordError.textContent = "";
-    }
-    else {
-        passwordError.textContent = "Password format invalid";
-    }    
-});
-
-const email = document.querySelector("#email");
-const emailError = document.querySelector(".email-error");
-email.addEventListener('input', function(){
-    let emailRegex = RegExp("^[a-z\\d+_.-]+@[a-z\\d.-]+$");
-    if (emailRegex.test(email.value)) {
-        emailError.textContent = "";
-    } else {
-        emailError.textContent = "Email format invalid";
-    }
-} );
-
-const phone = document.querySelector("#tel");
-const phoneError = document.querySelector(".tel-error");
-phone.addEventListener('input', function(){
-    let phoneRegex = RegExp("[+91]+[\\d]{10}$");
-    if (phoneRegex.test(phone.value)) {
-        phoneError.textContent = "";
-    } else {
-        phoneError.textContent = "Invalid phone number";
-    }
-});
-
-const salary = document.querySelector("#salary");
-const output = document.querySelector(".salary-output");
-salary.addEventListener('input', function () {
-    output.textContent = salary.value;
-});
+}
