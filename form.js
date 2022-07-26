e07423edca067c4f124a4f45b44d746de0a697b8
Inamul <inamulhoque097@gmail.com>
2022-07-26 13:01:06 +0530

    Added: Name validation

--- form.js
+++ form.js
@@ -2,10 +2,7 @@ function validation() {
     var text = document.getElementById("text").value;
     var password = document.getElementById("pwd").value;
     var mail = document.getElementById("email").value;
-    var num = document.getElementById("number").value;
     var phone = document.getElementById("tel").value;
-    var salary = document.getElementById("salary").value;
-    var dateTime = document.getElementById("party").value;
 
     var textRegex = /"^[A-Z]{1}[a-z]{2,}$"/;
     if (!textRegex.test(text)) {
