1d7139caff74b0a6544033594febe1036ca15cd9
Inamul <inamulhoque097@gmail.com>
2022-07-26 13:03:27 +0530

    Added: Phone number validation

--- form.js
+++ form.js
@@ -1,32 +1,11 @@
-const text = document.querySelector("#text");
-const textError = document.querySelector(".text-error");
-text.addEventListener('input', function () {
-    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,)$");
+function validation() {
+    var text = document.getElementById("text").value;
+    var password = document.getElementById("pwd").value;
+    var mail = document.getElementById("email").value;
+    var phone = document.getElementById("tel").value;
 
-    if (nameRegex.test(text.value)) {
-        textError.textContent = "";
+    var phoneRegex = /[+91]+[\\d]{10}$/;
+    if (!phoneRegex.test(phone)) {
+        alert("Invalid phone number."+" Length should be=10");
     }
-    else textError.textContent = "Name is Incorrect";
-});
-
-const email = document.querySelector("#email");
-const emailError = document.querySelector(".email-error");
-email.addEventListener('input', function(){
-    let emailRegex = RegExp("^[a-z\\d+_.-]+@[a-z\\d.-]+$");
-    if (emailRegex.test(email.value)) {
-        emailError.textContent = "";
-    } else {
-        emailError.textContent = "Email format invalid";
-    }
-} );
-
-const phone = document.querySelector("#tel");
-const phoneError = document.querySelector(".tel-error");
-phone.addEventListener('input', function(){
-    let phoneRegex = RegExp("[+91]+[\\d]{10}$");
-    if (phoneRegex.test(phone.value)) {
-        phoneError.textContent = "";
-    } else {
-        phoneError.textContent = "Invalid phone number";
-    }
-});
+}
