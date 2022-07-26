5e52c8d9a61ac37463790ade56aae11dae1bd547
Inamul <inamulhoque097@gmail.com>
2022-07-26 12:36:33 +0530

    Added: Name validation

--- form.js
+++ form.js
@@ -1,11 +1,14 @@
-const text = document.querySelector("#text");
-const textError = document.querySelector(".text-error");
+function validation() {
+    var text = document.getElementById("text").value;
+    var password = document.getElementById("pwd").value;
+    var mail = document.getElementById("email").value;
+    var num = document.getElementById("number").value;
+    var phone = document.getElementById("tel").value;
+    var salary = document.getElementById("salary").value;
+    var dateTime = document.getElementById("party").value;
 
-text.addEventListener('input', function () {
-    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,)$");
-
-    if (nameRegex.test(text.value)) {
-        textError.textContent = "";
+    var textRegex = /"^[A-Z]{1}[a-z]{2,}$"/;
+    if (!textRegex.test(text)) {
+        alert("Invalid Name format."+" Should contain first upperCase, lowerCase and length >= 3");
     }
-    else textError.textContent = "Name is Incorrect";
-});
+}
